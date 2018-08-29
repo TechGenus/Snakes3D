6b23dc5cf057a22a0f1b2391d7dbf1d8037b815d
Sydney Watenya <swatenya@gmail.com>
2018-08-29 17:54:33 -0400

    mapped player 1 controls to joystick

--- Assets/Scripts/WinTextRestart.cs
+++ Assets/Scripts/WinTextRestart.cs
@@ -8,7 +8,7 @@ public class WinTextRestart : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.R)) {
+		if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown("joystick button 7") ){
 			SceneManager.LoadScene(nameOfSceneToLoad);
 		}
 	}
